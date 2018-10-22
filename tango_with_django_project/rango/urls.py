c801ff809779fa1358d655aefbb208f55a05b8ad
Suraj-K-Dubey <surajdubey302@gmail.com>
2018-10-22 16:35:33 +0530

    Completed

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,6 +4,10 @@ from rango import views
 urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'about/', views.about, name='about'),
-    re_path(r'category/(?P<category_name_url>\w+)',
-            views.category, name='category')
+    re_path(r'category/(?P<category_name_url>\w+)$',
+            views.category, name='category'),
+    re_path(r'category/(?P<category_name_url>\w+)/add_page/$',
+            views.add_page, name="add_page"),
+    re_path(r'^add_category/$', views.add_category,
+            name='add_category')
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+
 
 # Create your views here.
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
+    category_list = Category.objects.order_by('likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    context_dict = {'boldmessage': "I am Haguchi"}
     context_dict.update({'categories': category_list, 'pages': page_list})
     for category in category_list:
         category.url = category.name.replace(' ', '_')
@@ -21,7 +23,8 @@ def about(request):
 
 def category(request, category_name_url):
     category_name = category_name_url.replace('_', ' ')
-    context_dict = {'category_name': category_name}
+    context_dict = {'category_name': category_name,
+                    'category_name_url': category_name_url}
     try:
         category = Category.objects.get(name=category_name)
         pages = Page.objects.filter(category=category)
@@ -30,3 +33,46 @@ def category(request, category_name_url):
     except Category.DoesNotExist:
         pass
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('index')
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+    return render(request, 'rango/add_category.html', { 'form': form })
+
+
+def add_page(request, category_name_url):
+    category_name = decode_url(category_name_url)
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            page = form.save(commit=True)
+            try:
+                cat = Category.objects.get(name=category_name)
+                page.category = cat
+            except Category.DoesNotExist:
+                return render(request, 'rango/add_category.html', {})
+            page.views = 0
+            page.save()
+            return category(request, category_name_url)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+    form = PageForm()
+    return render(request, 'rango/add_page.html',
+                  {'form': form,
+                   'category_name_url': category_name_url,
+                   'category_name': category_name})
+
+
+def decode_url(category_name_url):
+    category_name = category_name_url.replace('_', ' ')
+    return category_name
